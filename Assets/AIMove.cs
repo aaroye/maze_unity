c6f8f33e94e16ed69058986ea0fc659be35528f0
Royeee <b10715039@gapps.ntust.edu.tw>
2020-05-27 15:01:47 +0800

    Navigation bug

---
+++ Assets/AIMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMove : MonoBehaviour
+{
+    public Transform following;
+    private NavMeshAgent nav;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(following.position);
+    }
+}
